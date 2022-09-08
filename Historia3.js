f9095a5b568170e13738dff2656afcd7fdb8c2a5
WesleyLinux <56045146+wesleyMirand@users.noreply.github.com>
2022-09-08 00:26:54 -0300

    Update Historia3.js

--- Historia3.js
+++ Historia3.js
@@ -19,7 +19,48 @@
                         
                         1995- criamento de uma linguagem pra usar o "Html", mesma epoca do "java", surgiu "javascript", internet explorer,
                         
-                        1997- 
+                        1997- Netscape queria pradonizar a linguagem e pediu ajuda da "ECMA":paroniza as coisas. ECMASCRIPT/ é o js padronizado.
+                        
+                        2002- netscape faliu, explorer liderou, galera da net não desistiu e lançou o "MOZILA"-> depois "Firefox"
+                        
+                        2008- Surgiu o Google Chrome
+                        
+                        2009- surgiu um motor internos v8, que roda atras do google chrome, que roda JS. v8 gerava codigo : Jit ou Just in Time. 
+                        v8 é codigo livre, aberto.
+                        
+                        2010- surgiu O node Js, maquina que roda JS, fora do navegador, (nas antiga js foi feito pra rodar so no navegador) mais
+                        teve um iniciativa para o js sai do navegadores, que ai ele foi pro NODEJS que deixa o JS do lado do Servidor.
+                        
+                        O ecma script:
+                        1997 - versao 1.0 1998 - versão 2.0 atualizada 
+                        1999 - 3.o atualizada trouxe compatibilidade com expressões regulares, trouxe  o bloco try/cath para tratar erros, que ja tinha
+                        nas linguagem c e java. 
+                        ai so em 
+                        2009 - surgiu o ES5  mais popular de todas e compativel com todos navegadores, se torno compativel com "JSON" e 
+                        trouxe metodos pra tratar "Arrays".
+                        
+                        2015- surgiu o ES6, trouxe declaradores de "LET e CONST" e "Strings" templates,trouxe  funcão de operador de exponenciação
+                        aparti da versão 7 começa a se chama pelo ano exemplo ES2017
+                        2017 - trouxe  funcão "assincronas"
+                        
+                        2018- Es2018, surgiu as expressoes RegEx, trouxe  promises(promesas).
+                        etc..
+                        Varias ferramentas surgiram do "JavaScript", react ...., 
+                        Um ferramenta de conjunto a biblioteca que usa muito  "JS" é o "Jquery"-criado pela galera do mozila
+                        
+                        2009 - surgiu o angular, criado pelo "google", facilita app Web.Lingaguem menos imperativa, ela e mais declarativa,
+                        semelhante ao SQL banco de dados.
+                        O face lancou sua biblioteca o REACT, com algumas função do angular.
+                        React Native criação de app e celular.
+                        
+                        2014- criado o "Vue.js" e tambem surgiu o "ELECTON" mantido pela "GitHUb" especializado em interfaces graficas, programas...
+                        Visual Studio foi feito em "Electon", "Discord" tambem, "SLACK".
+                        
+                        O Ionic , surgiu do JS, usa o NODE.JS, mais é uma "SDK" para criar aplicação para dispositivo moveis, 
+                        veio de "PhoneGap" versao antiga na tecnologia, evoluiu e esta na mao da "APACHE" se chama "CORDOVA". */
+                     
+                        
+                       
                         
                         
                         
