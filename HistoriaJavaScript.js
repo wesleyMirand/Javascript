3e364b452325eb9cd54e509307a143aa78d01ab8
WesleyLinux <56045146+wesleyMirand@users.noreply.github.com>
2022-09-07 23:10:38 -0300

    Create HistoriaJavaScript.js

---
+++ HistoriaJavaScript.js
@@ -0,0 +1,32 @@
+                                            /*   ´${  Oque javaScript faz? }´
+                                                 
+                                                 
+          O (Javascript) é conhecido, por fazer tudo na web, essa linguagem vem do "Client x Servidor". O computador e o celulares são os clientes, 
+          que utilizam serviço fornecido por servidores, que são máquinas de (alta performance) que fornecem os dados. Quando se usa um app, você está
+          sendo o cliente, que precisa dos dados do "Servidor". E pra fazer o acesso via internet, se usa o navegador. 
+          exemplo:
+           
+           WWW.Jsamagia.com.br/aula1 ---> Ao digitar agente chama a "URL --> Vai para estrutura da internet --> Ela responde qual é o servidor--> 
+                                           --> Ser desviado --> nuvem Internet --> Fornece o servidor.
+                                           
+          O servidor é uma máquina de maneira fundamental, tem sistema operacional, pastas, diretórios internos. Dentro das pastas tem arquivos, que
+          pode ser o solicitado, exemplo: foto, texto, video, Doc Html...
+          
+          --> ao enviar pro servidor ele manda uma cópia --> pega o arquivo --> por cópia --> mandar essa cópia diretamente para o navegador(explorer)...
+          
+          Aparti dai o "Navegador" começa a trabalhar, para gerar o efeito visual, no que está vendo. 
+                                                                                                      
+          Um "WebSite" é composto, por três tecnologias importantes:(1)O Usúario,(2)Designer,(3)Programador--> vai cuidar da engenharia, configuracao, integração
+                                                                      /              ->>>>>>> Ai entra o
+                                                                     /                       Designer cuidado da interace visual.
+             conteudo-designer-interação                            /                         Organiza cores, tamanho, txto
+                                                                   > é responsável,
+                                                                      junto com a equipe,
+                                                                      faz oque deve ser feito
+           Analista/Html(1), Designer/css(2), Programador/ Js(3)                                                  
+            usúario          cuida do style      interação 
+          
+          As três tecnologias, funcionam em conjunto, compondo os clientes. */
+                
+                                                                      
+        
