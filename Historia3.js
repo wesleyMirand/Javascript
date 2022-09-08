dcc912115843d2ad6be36c7474f37264cbe215fd
WesleyLinux <56045146+wesleyMirand@users.noreply.github.com>
2022-09-07 23:56:20 -0300

    Create Historia3.js

---
+++ Historia3.js
@@ -0,0 +1,27 @@
+ /*
+                        1.Qual diferença entre cliente e um servidor na internet?
+                        o CLIENTE É pC,cELL, TUDO QUE USARO O NAVEGADOR. SERVIDOR MAQUINA DE ALTA PERFOMANCE, FORNECE DADOS.
+                        2.Para que serve Html,Css e Js?
+                        r: Html(Usuario, escreve texto), Css( muda cor, fonte) interpretação de texto. Js(programador) linguagem de programação.
+                        3. 3 Sites que usam js?
+                        R: Google, yOUTUBE, TWITTER
+                        */
+                        ´${EVOLUÇÃO JS}´
+                        
+                        /* JAVA & JAVASCRIPT SÃO DIFERENTE?
+                        
+                        1970- começo de tudo, tecnologia se inovando entre as guerres e pesquisas de guerra, dados salvos, atualizações"DARPA, Arpanet".
+                        
+                        1993- surgimento "html", protocolo http "Protoco tcp/ip", fundo "www- world wide web", "mosaic" primeiro interpretador funcionava
+                        com http e mostrava paginas Web.
+                        
+                        1994- fundação da "NETSCAPE" navegador tipo o "MOSAIC"
+                        
+                        1995- criamento de uma linguagem pra usar o "Html", mesma epoca do "java", surgiu "javascript", internet explorer,
+                        
+                        1997- 
+                        
+                        
+                        
+                      
+                        
