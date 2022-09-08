1343790c56bfdda82ce4f207afcb83fdd6d6c3a1
WesleyLinux <56045146+wesleyMirand@users.noreply.github.com>
2022-09-07 23:37:23 -0300

    Create Historia2.js

---
+++ Historia2.js
@@ -0,0 +1,34 @@
+                                                            /* Oque fazer pra começar?
+                                                            
+                                                            
+                (1) titulo (2) gerar texto & imagens & paragrafos bem organizado"HTML" 
+                entrega todo doc pro DESIGNER, tudo legivel. (3) "INTERAÇÃO" apareça texto, legenda, clicar img, button.
+                
+                HTML, CSS: Interpretação de TEXTO
+                JS: Linguagem
+                
+                QUEM USA? R: GOOGle, Youtube, Linkedin, Netflix, Facebook etc...
+                
+                exemplo: Pesquisar algo no google ao percebe um ajuda pra escrever a palavra, isso é o javascript no dia dia, rola imagens,
+                Sem Js não usa o Youtube. */
+
+                Alert ("Olá, Github")
+                 ^basico
+                /*cor fundo*/
+                document.body.style.background = 'black'
+                  /* ^ analisa o estylo do doc ^*/
+              
+                document.querySelector('img#hplogo').style.filter = 'hrayscale(100%)'
+                  /* mudar logo do google ^ */
+
+                 document.querySelector('img#hplogo').style.visibility = "hiddem"
+                  /* sumir com o logo do google ^^ */
+
+                 /*Acima foi alterado o javascript, que já existe em cada máquina. */
+
+                 
+                 document.querySelector ('span.bstn-hl-title').innerText = "Curso Wewe"
+                 /* acima muda o titulo de algum site */
+
+                 
+                 
